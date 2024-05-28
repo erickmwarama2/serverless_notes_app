3a8768116f85c7ad359d83df4ffb280d26e1ac03
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-05-28 10:15:57 +0300

    add get notes method

--- notes-backend/api/get-notes.js
+++ notes-backend/api/get-notes.js
@@ -8,10 +8,35 @@ const tableName = process.env.NOTES_TABLE;
 exports.handler = async function(event) {
     try {
 
+        let query = event.queryStringParameters;
+        let limit = query && query.limit ? parseInt(query.limit) : 5;
+        let user_id = util.getUserId(event.headers);
+
+        let params = {
+            TableName: tableName,
+            KeyConditionExpression: "user_id = :uid",
+            ExpressionAttributeValues: {
+                ':uid': user_id
+            },
+            Limit: limit,
+            ScanIndexForward: false
+        };
+
+        let startTimestamp = query && query.start ? parseInt(query.start) : 0;
+
+        if (startTimestamp > 0) {
+            params.ExclusiveStartKey = {
+                user_id,
+                timestamp: startTimestamp
+            }
+        }
+
+        let data = await dynamodb.query(params).promise();
+
         return {
             statusCode: 200,
             headers: util.getResponseHeaders(),
-            body: JSON.stringify({})
+            body: JSON.stringify(data)
         }
     } catch (error) {
         console.log("Error", error);
--- notes-backend/api/update-note.js
+++ notes-backend/api/update-note.js
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'eu-west-2'});
 const util = require('./util.js');
+const moment = require('moment');
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.NOTES_TABLE;
@@ -8,10 +9,27 @@ const tableName = process.env.NOTES_TABLE;
 exports.handler = async function(event) {
     try {
 
+        let item = JSON.parse(event.body).Item;
+        item.user_id = util.getUserId(event.headers);
+        item.user_name = util.getUserName(event.headers);
+        item.expires = moment().add(15, 'days').unix();
+
+        let data = await dynamodb.put({
+            TableName: tableName,
+            Item: item,
+            ConditionExpression: '#t = :t',
+            ExpressionAttributeNames: {
+                '#t': 'timestamp'
+            },
+            ExpressionAttributeValues: {
+                ':t': item.timestamp
+            }
+        }).promise();
+
         return {
             statusCode: 200,
             headers: util.getResponseHeaders(),
-            body: JSON.stringify({})
+            body: JSON.stringify(item)
         }
     } catch (error) {
         console.log("Error", error);
