11ff02d8f7cd16eb01f09015be31c7671f17fce8
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-05-28 10:37:08 +0300

    add crud operations

--- notes-backend/api/delete-note.js
+++ notes-backend/api/delete-note.js
@@ -8,11 +8,22 @@ const tableName = process.env.NOTES_TABLE;
 exports.handler = async function(event) {
     try {
 
+        let timestamp = parseInt(event.pathParameters.timestamp);
+
+        let params = {
+            TableName: tableName,
+            Key: {
+                user_id: util.getUserId(event.headers),
+                timestamp
+            }
+        };
+
+        await dynamodb.delete(params).promise();
+
         return {
             statusCode: 200,
-            headers: util.getResponseHeaders(),
-            body: JSON.stringify({})
-        }
+            headers: util.getResponseHeaders()
+        };
     } catch (error) {
         console.log("Error", error);
 
--- notes-backend/api/get-note.js
+++ notes-backend/api/get-note.js
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'eu-west-2'});
 const util = require('./util.js');
+const _ = require('underscore');
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.NOTES_TABLE;
@@ -8,11 +9,33 @@ const tableName = process.env.NOTES_TABLE;
 exports.handler = async function(event) {
     try {
 
-        return {
-            statusCode: 200,
-            headers: util.getResponseHeaders(),
-            body: JSON.stringify({})
+        let note_id = decodeURIComponent(event.pathParameters.note_id);
+
+        let params = {
+            TableName: tableName,
+            IndexName: "note_id-index",
+            KeyConditionExpression: "note_id = :note_id",
+            ExpressionAttributeValues: {
+                ':note_id': note_id
+            },
+            Limit: 1
+        };
+
+        let data = await dynamodb.query(params).promise();
+
+        if (!_.isEmpty(data.Items)) {
+            return {
+                statusCode: 200,
+                headers: util.getResponseHeaders(),
+                body: JSON.stringify(data.Items[0])
+            }
+        } else {
+            return {
+                statusCode: 404,
+                headers: util.getResponseHeaders(),
+            }
         }
+
     } catch (error) {
         console.log("Error", error);
 
